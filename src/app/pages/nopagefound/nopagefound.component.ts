f182010322e982176807960c414edcb3f79a1760
miguel <0shiromiguel@gmail.com>
2021-08-03 14:01:22 -0500

    fin seccion 3 - diseño

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styles: [
+  ]
+})
+export class NopagefoundComponent {
+
+  year = new Date().getFullYear();
+
+}
