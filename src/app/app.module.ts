da7fd5dfcf8ce0d9da6d8b79d6b44aac15ad1304
miguel <0shiromiguel@gmail.com>
2021-08-12 12:00:33 -0500

    Fin seccion 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
   declarations: [
     AppComponent,
     NopagefoundComponent,
+
   ],
   imports: [
     BrowserModule,
